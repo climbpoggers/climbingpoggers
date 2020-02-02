030dff73222f7459319241cccbf4465bbde72b9f
Daniel Combs <danielreedcombs@gmail.com>
2020-02-01 18:31:32 -0600

    hello world

---
+++ climbingpoggers/src/ClimbTracker.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+export default class ClimbTracker extends Component{
+    state = {}
+
+    render(){
+        return(
+        <div>
+            <p>HELLO WORLD</p>
+        </div>)
+    }
+}
